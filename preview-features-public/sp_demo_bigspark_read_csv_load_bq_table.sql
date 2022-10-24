52e30977d36f43c72a5b782742e131463f62eadf
paternostro <paternostro@google.com>
2022-10-24 14:48:54 -0400

    Added Next 22 public preview features

---
+++ preview-features-public/sp_demo_bigspark_read_csv_load_bq_table.sql
@@ -0,0 +1,110 @@
+CREATE OR REPLACE PROCEDURE
+	`bigquery_preview_features.sp_demo_bigspark_read_csv_load_bq_table`(test_parameter STRING)
+WITH CONNECTION `us.bigspark-connection` OPTIONS (engine='SPARK',
+		jar_uris=["gs://sample-shared-data/bigspark/spark-bigquery-with-dependencies_2.12-0.26.0.jar"])
+	LANGUAGE python AS R"""
+####################################################################################
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+####################################################################################
+
+
+# Use Cases:
+#    - Be able to directly run Spark in BigQuery
+#    - Supplement BigQuery functionality with Spark
+#
+# Description: 
+#     - First View and Explore the Cloud Storage Account in this Project
+# 
+# Show:
+#     - BQ support for Spark
+#     - Running spark directly in the interface
+# 
+# References:
+#     - pending
+# 
+# Clean up / Reset script:
+#   DROP EXTERNAL TABLE IF EXISTS `bigquery_preview_features.product_discounts`;
+
+# To invoke:
+#   CALL `bigquery_preview_features.sp_demo_bigspark_read_csv_load_bq_table`('testval');
+
+# To create the External Connection (you can use the BQ UI for this under "+ Add Data | External Data Source")
+# bq mk --connection \
+#       --connection_type='SPARK' \
+#       --project_id="data-analytics-golden-v1-share" \
+#       --location="us" \
+#       "bigspark-connection"
+
+# NOTE: You must enable Dataproc API in the project for BigSpark (this has been done)
+
+# NOTE: The storage buckets must be accessible via the service account on the external connection
+#       View the connection to see the service account
+#       e.g. bqcx-312090430116-oi5j@gcp-sa-bigquery-consp.iam.gserviceaccount.com 
+#       This account was granted Editor role at the Project level due to Preview Requirements
+
+# NOTE: You cannot edit a Spark SP at this point (seems like parameters are not codegening correctly)
+# Proper CREATE OR REPLACE STATMENT (there is no BEGIN/END for this):
+#   CREATE OR REPLACE PROCEDURE bigquery_preview_features.sp_demo_bigspark_read_csv_load_bq_table (test_parameter STRING)
+#     WITH CONNECTION `us.bigspark-connection`
+#     OPTIONS(engine="SPARK", jar_uris=["gs://sample-shared-data/bigspark/spark-bigquery-with-dependencies_2.12-0.26.0.jar"]) 
+#     LANGUAGE python AS r{{3 double quotes}}
+
+# NOTE: To see the orginal SQL, View the Saved Queries | Project Queries | sp_demo_bigspark_read_csv_load_bq_table
+
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField,  IntegerType, DoubleType, StringType
+import os
+import json
+test_parameter_value = str(json.loads(os.environ["BIGQUERY_PROC_PARAM.test_parameter"]))
+print("test_parameter_value", test_parameter_value)
+
+
+print("Create session")
+spark = SparkSession.builder \
+    .config("spark.jars.packages","com.google.cloud.spark:spark-bigquery-with-dependencies_2.12-0.26.0") \
+    .appName('bq_spark_sp_demo_bigspark_read_csv_load_bq_table') \
+    .getOrCreate()
+
+spark.conf.set("temporaryGcsBucket","sample-shared-data-temp")
+
+print("Declare schema of file to load")
+# SKU,brand,department,discount,discount_code
+discountSchema = StructType([
+    StructField('SKU', StringType(), False),
+    StructField('brand', StringType(), False),
+    StructField('department', StringType(), False),
+    StructField('discount', IntegerType(), False),
+    StructField('discount_code', StringType(), False)])
+    
+print("Load the discount")
+dfDiscount = spark.read.format("csv") \
+    .option("header", True) \
+    .option("delimiter", ",") \
+    .schema(discountSchema) \
+    .load('gs://sample-shared-data/bigspark/sample-bigspark-discount-data.csv')
+ 
+print("Determine if discount is High, Medium or Low")
+dfDiscount = dfDiscount.withColumn("discount_rate", F.expr("CASE WHEN discount < 10 THEN 'Low' WHEN discount < 20 THEN 'Medium' ELSE 'High' END"))
+
+print("Display dataframe")
+dfDiscount.show(5)
+
+print("Saving results to BigQuery")
+dfDiscount.write.format("bigquery") \
+    .option("table", "bigquery_preview_features.product_discounts") \
+    .mode("overwrite") \
+    .save()
+""";
---
+++ preview-features-public/sp_demo_caspian.sql
@@ -0,0 +1,422 @@
+CREATE OR REPLACE PROCEDURE `bigquery_preview_features.sp_demo_caspian`()
+BEGIN
+/*##################################################################################
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     https://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###################################################################################*/
+
+  
+/*
+Use Cases:
+    - Secure your files on GCS with data goverance (row, column level security)
+      Users do not need access to storage (they only need access to BigQuery)
+    - Provide SQL access to your files
+    - Allow BigQuery to cache your GCS metadata (file listing)
+
+Description: 
+    - First View and Explore the Cloud Storage Account in this Project
+      - The bucket is named "sample-shared-data"
+      - The first folder "bigquery-caspian-tf-models" contains TensorFlow models download from TensorFlow Hub: https://tfhub.dev/
+      - The second folder "bigquery-caspian" contains the images we will place a BigQuery table over
+
+Show:
+    - BQ support for GCS
+    - BQ importing models
+    - BQ scoring image data using a model
+
+References:
+    - go/caspian-user-guide
+
+Clean up / Reset script:
+    DROP EXTERNAL TABLE IF EXISTS `bigquery_preview_features.biglake_caspian`;
+    DROP MODEL IF EXISTS `bigquery_preview_features.resnet_50_classification_1`;
+    DROP MODEL IF EXISTS `bigquery_preview_features.imagenet_mobilenet_v3_small_075_224_feature_vector_5`;
+*/
+
+
+/*
+You need to make the connection to Cloud Storage
+Since this is preview you need to use Cloud Shell (BQ UI does not have these yet)
+
+This Connection has been created
+
+bq mk --connection \
+  --location="US" \
+  --project_id="data-analytics-golden-v1-share" \
+  --connection_type=CLOUD_RESOURCE \
+  "caspian-connection"
+
+bq show \
+  --location="US" \
+  --connection "caspian-connection"
+
+*/
+
+DECLARE loopCounter INT64 DEFAULT 0;
+
+-- Create a BigLake table for the bigquery-caspian directory for all JPG image files
+-- Review the OPTIONS block
+-- NOTE: You do not have access to run this command (the table is already created)
+--       If you want to run this use the dataset "ce_playground_google"
+CREATE OR REPLACE EXTERNAL TABLE `bigquery_preview_features.biglake_caspian`
+WITH CONNECTION `us.caspian-connection `
+OPTIONS (
+  object_metadata="DIRECTORY",
+  uris = ['gs://sample-shared-data/bigquery-caspian/*.jpg'],
+  max_staleness=INTERVAL 30 MINUTE, 
+  metadata_cache_mode="AUTOMATIC"
+  );
+
+
+-- View the data
+SELECT * FROM `bigquery_preview_features.biglake_caspian`;
+
+
+-- Download model from: https://tfhub.dev/tensorflow/resnet_50/classification/1
+-- Save in storage
+-- NOTE: You do not have access to run this command (the table is already created)
+--       If you want to run this use the dataset "ce_playground_google"
+CREATE OR REPLACE MODEL `bigquery_preview_features.resnet_50_classification_1` 
+OPTIONS(
+  model_type="TENSORFLOW", 
+  color_space="RGB", 
+  model_path="gs://sample-shared-data/bigquery-caspian-tf-models/resnet_50_classification_1/*"
+  );
+
+
+-- Score the image data
+SELECT * 
+  FROM ML.PREDICT(MODEL `bigquery_preview_features.resnet_50_classification_1`, 
+                  (SELECT * 
+                     FROM `bigquery_preview_features.biglake_caspian`
+                    WHERE content_type = 'image/jpeg'
+                    LIMIT 10)
+                  );
+
+
+-- Download model from: https://tfhub.dev/google/imagenet/mobilenet_v3_small_075_224/feature_vector/5
+-- Save in storage
+-- NOTE: You do not have access to run this command (the table is already created)
+--       If you want to run this use the dataset "ce_playground_google"
+CREATE OR REPLACE MODEL `bigquery_preview_features.imagenet_mobilenet_v3_small_075_224_feature_vector_5` 
+OPTIONS(
+  model_type="TENSORFLOW", 
+  color_space="RGB", 
+  model_path="gs://sample-shared-data/bigquery-caspian-tf-models/imagenet_mobilenet_v3_small_075_224_feature_vector_5/*"
+  );
+
+
+-- Score the image data
+SELECT * 
+  FROM ML.PREDICT(MODEL `bigquery_preview_features.imagenet_mobilenet_v3_small_075_224_feature_vector_5`, 
+                  (SELECT * 
+                     FROM `bigquery_preview_features.biglake_caspian`
+                    WHERE content_type = 'image/jpeg'
+                    LIMIT 10)
+                  );
+
+------------------------------------------------------------------------------------------
+-- External Cloud Functions that use the Vision API
+-- Create a connection to a cloud function that will use the Vertex AI to detect objects in images
+
+-- NOTE: You do not have access to run this command (the table is already created)
+--       If you want to run this use the dataset "ce_playground_google"
+------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION `bigquery_preview_features.ext_udf_ai_localize_objects` (uri STRING) RETURNS STRING 
+    REMOTE WITH CONNECTION `us.cloud-function` 
+    OPTIONS 
+    (endpoint = 'https://us-central1-cloudfunctions.net/bigquery_external_function', 
+    user_defined_context = [("mode","localize_objects_uri")]
+    );
+
+
+CREATE OR REPLACE FUNCTION `bigquery_preview_features.ext_udf_ai_detect_labels` (uri STRING) RETURNS STRING 
+    REMOTE WITH CONNECTION `us.cloud-function` 
+    OPTIONS 
+    (endpoint = 'https://us-central1-cloudfunctions.net/bigquery_external_function', 
+    user_defined_context = [("mode","detect_labels_uri")]
+    );
+
+
+CREATE OR REPLACE FUNCTION `bigquery_preview_features.ext_udf_ai_detect_landmarks` (uri STRING) RETURNS STRING 
+    REMOTE WITH CONNECTION `us.cloud-function` 
+    OPTIONS 
+    (endpoint = 'https://us-central1-cloudfunctions.net/bigquery_external_function', 
+    user_defined_context = [("mode","detect_landmarks_uri")]
+    );
+
+
+CREATE OR REPLACE FUNCTION `bigquery_preview_features.ext_udf_ai_detect_logos` (uri STRING) RETURNS STRING 
+    REMOTE WITH CONNECTION `us.cloud-function` 
+    OPTIONS 
+    (endpoint = 'https://us-central1-cloudfunctions.net/bigquery_external_function', 
+    user_defined_context = [("mode","detect_logos_uri")]
+    );
+
+
+------------------------------------------------------------------------------------------
+-- Use our BigLake table to do some image detection
+------------------------------------------------------------------------------------------
+-- Call the object_localization method of Vision API
+-- The Vision API can detect and extract multiple objects in an image with Object Localization.
+-- Object localization identifies multiple objects in an image and provides a LocalizedObjectAnnotation for each object in the image.
+-- https://cloud.google.com/vision/docs/object-localizer
+-- For more images: gsutil ls gs://cloud-samples-data/vision/object_localization/
+WITH Data AS
+(
+  SELECT uri
+    FROM `bigquery_preview_features.biglake_caspian`
+   WHERE content_type = 'image/jpeg'
+     AND STARTS_WITH(uri,'gs://sample-shared-data/bigquery-caspian/vision/object_localization/')
+  LIMIT 10
+)
+, ScoreAI AS 
+(
+  SELECT uri, SAFE.PARSE_JSON(`bigquery_preview_features.ext_udf_ai_localize_objects`(Data.uri)) AS json_result
+    FROM Data
+)
+SELECT uri,
+       item.name,
+       item.score,
+       json_result 
+ FROM  ScoreAI, UNNEST(JSON_QUERY_ARRAY(ScoreAI.json_result.localizedObjectAnnotations)) AS item;
+
+
+
+-- Call the label_detection method of Vision API
+-- The Vision API can detect and extract information about entities in an image, across a broad group of categories.
+-- Labels can identify general objects, locations, activities, animal species, products, and more.
+-- https://cloud.google.com/vision/docs/labels
+-- For more images: gsutil ls gs://cloud-samples-data/vision/label
+WITH Data AS
+(
+  SELECT uri
+    FROM `bigquery_preview_features.biglake_caspian`
+   WHERE content_type = 'image/jpeg'
+     AND STARTS_WITH(uri,'gs://sample-shared-data/bigquery-caspian/vision/label/')
+  LIMIT 10
+)
+, ScoreAI AS 
+(
+  SELECT uri, SAFE.PARSE_JSON(`bigquery_preview_features.ext_udf_ai_detect_labels`(Data.uri)) AS json_result
+    FROM Data
+)
+SELECT uri,
+       item.description,
+       item.score,
+       json_result 
+ FROM  ScoreAI, UNNEST(JSON_QUERY_ARRAY(ScoreAI.json_result.labelAnnotations)) AS item;
+
+
+-- Call the landmark_detection method of Vision API
+-- For more images: gsutil ls gs://cloud-samples-data/vision/landmark
+-- Landmark Detection detects popular natural and human-made structures within an image
+-- https://cloud.google.com/vision/docs/detecting-landmarks
+-- For more images: gsutil ls gs://cloud-samples-data/vision/landmark
+WITH Data AS
+(
+  SELECT uri
+    FROM `bigquery_preview_features.biglake_caspian`
+   WHERE content_type = 'image/jpeg'
+     AND STARTS_WITH(uri,'gs://sample-shared-data/bigquery-caspian/vision/landmark/')
+  LIMIT 10
+)
+, ScoreAI AS 
+(
+  SELECT uri, SAFE.PARSE_JSON(`bigquery_preview_features.ext_udf_ai_detect_landmarks`(Data.uri)) AS json_result
+    FROM Data
+)
+SELECT uri,
+       item.description,
+       item.score,
+       json_result 
+ FROM  ScoreAI, UNNEST(JSON_QUERY_ARRAY(ScoreAI.json_result.landmarkAnnotations)) AS item;
+
+
+-- Call the logo_detection method of Vision API
+-- Logo Detection detects popular product logos within an image.
+-- https://cloud.google.com/vision/docs/detecting-logos
+-- For more images: gsutil ls gs://cloud-samples-data/vision/logo
+WITH Data AS
+(
+  SELECT uri
+    FROM `bigquery_preview_features.biglake_caspian`
+   WHERE content_type = 'image/jpeg'
+     AND STARTS_WITH(uri,'gs://sample-shared-data/bigquery-caspian/vision/logo/')
+  LIMIT 10
+)
+, ScoreAI AS 
+(
+  SELECT uri, SAFE.PARSE_JSON(`bigquery_preview_features.ext_udf_ai_detect_logos`(Data.uri)) AS json_result
+    FROM Data
+)
+SELECT uri,
+       item.description,
+       item.score,
+       json_result 
+ FROM  ScoreAI, UNNEST(JSON_QUERY_ARRAY(ScoreAI.json_result.labelAnnotations)) AS item;
+
+
+-- You can also save the results in a BigQuery Table and now join the two 
+-- You could also have a cloud function that updates the metadata tags of the files in cloud storage
+
+
+-- How do we secure this data and share it?
+-- We can add metatags on the data 
+-- We can apply row level security on the data
+-- We can share the table with Analytics Hub
+-- We can generate Signed URLs so users can access the images
+-- NOTE: Signed URLs can be opened by anyone so becareful with who you share them with
+
+
+-- Create a new table (since we will be applying security and do not want to mess up the current one)
+CREATE OR REPLACE EXTERNAL TABLE `bigquery_preview_features.biglake_caspian_rls`
+WITH CONNECTION `us.caspian-connection `
+OPTIONS (
+  object_metadata="DIRECTORY",
+  uris = ['gs://sample-shared-data/bigquery-caspian/*.jpg'],
+  max_staleness=INTERVAL 30 MINUTE, 
+  metadata_cache_mode="AUTOMATIC"
+  );
+
+-- Some of the images contain sensitive information that we 
+-- View the files in https://console.cloud.google.com/storage/browser/sample-shared-data/bigquery-caspian/confidential?project=data-analytics-golden-v1-share
+-- These files have "Custom Metadata" Key=pii Value=true
+SELECT *
+  FROM `bigquery_preview_features.biglake_caspian` -- THE NON-SECURE TABLE (so we can see the data)
+ WHERE ARRAY_LENGTH(metadata) = 1 AND metadata[OFFSET(0)].name = 'pii' AND metadata[OFFSET(0)].value = 'true';
+
+
+-- Prevent anyone from see the data when we share
+-- Secure the data based upon the pii metadata
+CREATE OR REPLACE ROW ACCESS POLICY biglake_caspian_rls_pii
+    ON `bigquery_preview_features.biglake_caspian_rls`
+    GRANT TO ('allAuthenticatedUsers')
+FILTER USING (ARRAY_LENGTH(metadata) = 0 -- no meta data
+              OR
+              ARRAY_LENGTH(metadata) = 1 AND metadata[OFFSET(0)].name = 'pii' AND metadata[OFFSET(0)].value != 'true' -- contains pii tag, but not "true"
+              );
+
+
+-- Can we see it?  (should return nothing)
+SELECT *
+  FROM `bigquery_preview_features.biglake_caspian_rls` -- THE SECURE TABLE 
+ WHERE ARRAY_LENGTH(metadata) = 1 AND metadata[OFFSET(0)].name = 'pii' AND metadata[OFFSET(0)].value != 'true' ;
+
+-- We can see the rest of the data
+SELECT *
+  FROM `bigquery_preview_features.biglake_caspian_rls`;
+
+
+-- Users do not have access to the storage account (only BigLake), so how do they see / access the images
+-- Use the function EXTERNAL_OBJECT_TRANSFORM to generate a signed url
+-- Copy a signed_url and open anonymous browser and paste the url
+SELECT uri, 
+       content_type,
+       signed_url
+  FROM EXTERNAL_OBJECT_TRANSFORM (TABLE `bigquery_preview_features.biglake_caspian_rls`,['SIGNED_URL']);
+
+
+-- This table is also in the dataset named "data_analytics_shared_data"
+-- The table has been shared with Analytics Hub
+-- Open your Argolis, open Analytics Hub and click "Search Listings" (click off Private and select data_analytics_shared_data)
+-- You would run this SQL in your Argolis (you need to be allowlisted for the function EXTERNAL_OBJECT_TRANSFORM)
+/*
+SELECT uri, 
+       content_type,
+       signed_url
+  FROM EXTERNAL_OBJECT_TRANSFORM (TABLE `data_analytics_shared_data.biglake_caspian_rls`,['SIGNED_URL'] LIMIT 10);
+*/
+
+
+-- Create a table to hold all the ML scoring results
+-- NOTE: You do not have access to run this command (the table is already created)
+--       If you want to run this use the dataset "ce_playground_google"
+CREATE OR REPLACE TABLE `bigquery_preview_features.biglake_vision_ai`
+(
+  uri                        STRING,
+  vision_ai_localize_objects JSON,
+  vision_ai_detect_labels    JSON,
+  vision_ai_detect_landmarks JSON,
+  vision_ai_detect_logos     JSON
+)
+CLUSTER BY uri;
+
+-- Score all the data in batches so we do not overwelm the function
+-- Typically you would score the data as it arrives or in small batches
+-- This takes a while, so you project do not want to run (plus you might affect other users)
+LOOP
+  INSERT INTO `bigquery_preview_features.biglake_vision_ai`
+  (uri, vision_ai_localize_objects, vision_ai_detect_labels, vision_ai_detect_landmarks, vision_ai_detect_logos)
+  WITH Data AS
+  (
+    SELECT uri, 
+          ROW_NUMBER() OVER (ORDER BY updated) AS RowNumber
+      FROM `bigquery_preview_features.biglake_caspian`
+    WHERE content_type = 'image/jpeg'
+  )
+  SELECT uri, 
+        SAFE.PARSE_JSON(`bigquery_preview_features.ext_udf_ai_localize_objects`(uri)),
+        SAFE.PARSE_JSON(`bigquery_preview_features.ext_udf_ai_detect_labels`(uri)),
+        SAFE.PARSE_JSON(`bigquery_preview_features.ext_udf_ai_detect_landmarks`(uri)),
+        SAFE.PARSE_JSON(`bigquery_preview_features.ext_udf_ai_detect_logos`(uri)),
+    FROM Data
+    WHERE RowNumber BETWEEN (loopCounter + 1) AND (loopCounter + 100);
+
+  SET loopCounter = loopCounter + 100;
+  IF loopCounter >= 8200 THEN
+     LEAVE;
+  END IF;
+END LOOP;
+
+
+-- Create a search index on all columns
+-- The table is too small to benefit from a BigSearch index (this would be good for a larger dataset)
+-- NOTE: You do not have access to run this command (the table is already created)
+--       If you want to run this use the dataset "ce_playground_google"
+-- CREATE SEARCH INDEX idx_biglake_vision_ai
+-- ON `bigquery_preview_features.biglake_vision_ai` (ALL COLUMNS);
+
+-- Check our index (index_status = "Active", "total storage bytes" = 1.1 TB)
+-- See the unindexed_row_count (rows not indexed)
+-- coverage_percentage must be 100% to we know the data has been indexed
+-- SELECT * -- table_name, index_name, ddl, coverage_percentage
+--   FROM `bigquery_preview_features.INFORMATION_SCHEMA.SEARCH_INDEXES`
+--  WHERE index_status = 'ACTIVE';
+
+
+ -- Find all Cats, Dogs, etc...
+SELECT *
+  FROM `bigquery_preview_features.biglake_vision_ai` AS Data
+ WHERE SEARCH(Data, 'Whiskers');
+
+
+-- Find all Companion dogs
+SELECT *
+  FROM `bigquery_preview_features.biglake_vision_ai` AS Data
+ WHERE SEARCH(Data, 'Companion dog');
+
+
+-- Get a signed url for the search results
+SELECT Signed_Table.signed_url,
+       AI_Results.*
+  FROM EXTERNAL_OBJECT_TRANSFORM 
+          (TABLE `bigquery_preview_features.biglake_caspian`,['SIGNED_URL']) AS Signed_Table
+       INNER JOIN `bigquery_preview_features.biglake_vision_ai` AS AI_Results
+               ON Signed_Table.uri = AI_Results.uri
+              AND SEARCH(AI_Results, 'Companion dog');
+
+
+END;
