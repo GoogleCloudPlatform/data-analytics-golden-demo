e8a7e128c5b351f98bb545af3abc12eb3017c82e
paternostro <paternostro@google.com>
2023-06-28 11:13:24 -0400

    Updates for video links

--- cloud-composer/data/terraform/bucket-demo/terraform.tf
+++ cloud-composer/data/terraform/bucket-demo/terraform.tf
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ####################################################################################
+# YouTube: https://youtu.be/2Qu29_hR2Z0
 
 
 ####################################################################################
