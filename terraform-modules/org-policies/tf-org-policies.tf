46bce23c2ad20125f2ba804fb65f9dd16a8eb3f4
paternostro <paternostro@google.com>
2023-09-21 17:41:40 -0400

    Original one

--- terraform-modules/org-policies/tf-org-policies.tf
+++ terraform-modules/org-policies/tf-org-policies.tf
@@ -14,6 +14,7 @@
 # limitations under the License.
 ####################################################################################
 
+
 ####################################################################################
 # Deploys the Org Policies when running locally or cloud shell
 #
@@ -40,14 +41,7 @@ variable "project_id" {}
 ####################################################################################
 # Organizational Policies 
 ####################################################################################
-#
-# This file is empty on purpose.  We will run the Terraform apply command a second time with this file.
-# By running terraform apply with this empty file it will "destroy" the org policies.
-# This will set them back to "Inherit from Parent".
-#
-
-
-# Composer Policy and Dataproc Serverless { this needs to remain until the Dataproc team changes their code }
+# Composer Policy
 # This fixes this Error: googleapi: Error 400: You can't create a Composer environment due to Organization Policy constraints in the selected project.
 # Policy constraints/compute.requireOsLogin must be disabled., failedPrecondition
 resource "google_org_policy_policy" "org_policy_require_os_login" {
@@ -84,7 +78,8 @@ resource "google_org_policy_policy" "org_policy_require_os_login" {
 # }
 
 
-# Needed for Colab Enterprise notebooks
+# Error: Error waiting for creating Dataproc cluster: Error code 9, message: Constraint constraints/compute.requireShieldedVm violated for project projects/big-query-demo-09. Secure Boot is not enabled in the 'shielded_instance_config' field. 
+# See https://cloud.google.com/resource-manager/docs/organization-policy/org-policy-constraints for more information.
 resource "google_org_policy_policy" "org_policy_require_shielded_vm" {
   name     = "projects/${var.project_id}/policies/compute.requireShieldedVm"
   parent   = "projects/${var.project_id}"
@@ -98,7 +93,6 @@ resource "google_org_policy_policy" "org_policy_require_shielded_vm" {
 
 
 # To deploy the cloud function
-/*
 resource "google_org_policy_policy" "org_policy_allowed_ingress_settings" {
   name     = "projects/${var.project_id}/policies/cloudfunctions.allowedIngressSettings"
   parent   = "projects/${var.project_id}"
@@ -121,10 +115,9 @@ resource "google_org_policy_policy" "org_policy_allowed_ingress" {
     }
   }
 }
-*/
+
 
 # To set service accounts (since sometimes they cause a voliation)
-/*
 resource "google_org_policy_policy" "org_policy_allowed_policy_member_domains" {
   name     = "projects/${var.project_id}/policies/iam.allowedPolicyMemberDomains"
   parent   = "projects/${var.project_id}"
@@ -135,9 +128,8 @@ resource "google_org_policy_policy" "org_policy_allowed_policy_member_domains" {
     }
   }
 }
-*/
 
-/*
+
 # For Datastream to create the peer network
 resource "google_org_policy_policy" "org_policy_allowed_vpc_peering" {
   name     = "projects/${var.project_id}/policies/compute.restrictVpcPeering"
@@ -149,7 +141,7 @@ resource "google_org_policy_policy" "org_policy_allowed_vpc_peering" {
     }
   }
 }
-*/
+
 
 ####################################################################################
 # Time Delay for Org Policies
@@ -157,7 +149,6 @@ resource "google_org_policy_policy" "org_policy_allowed_vpc_peering" {
 # https://registry.terraform.io/providers/hashicorp/time/latest/docs/resources/sleep
 # The org policies take some time to proprogate.  
 # If you do not wait the below resource will fail.
-/*
 resource "time_sleep" "time_sleep_org_policies" {
   create_duration = "90s"
 
@@ -165,7 +156,8 @@ resource "time_sleep" "time_sleep_org_policies" {
     google_org_policy_policy.org_policy_require_os_login,
     google_org_policy_policy.org_policy_require_shielded_vm,
     google_org_policy_policy.org_policy_allowed_ingress_settings,
-    google_org_policy_policy.org_policy_allowed_policy_member_domains
+    google_org_policy_policy.org_policy_allowed_ingress,
+    google_org_policy_policy.org_policy_allowed_policy_member_domains,
+    google_org_policy_policy.org_policy_allowed_vpc_peering
   ]
 }
-*/
