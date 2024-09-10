5cbc62b488490c35c65ca6b9469b00307deba8f5
Adam Paternostro <adam.paternostro@outlook.com>
2024-09-10 10:53:47 -0400

    Snowflake script

--- snowflake-iceberg.sql
+++ snowflake-iceberg.sql
@@ -1,13 +1,15 @@
--- Warehouse
+-----------------------------------------------------------------------------------------------
+-- Run this on Snowflake
+-----------------------------------------------------------------------------------------------
+
+-- Warehouse (this is the compute needed to run Snowflake)
 -- https://docs.snowflake.com/en/sql-reference/sql/create-warehouse
 CREATE OR REPLACE WAREHOUSE ICEBERG_WAREHOUSE WITH WAREHOUSE_SIZE='XSMALL';
 
--- role
-CREATE ROLE iceberg_lab;
-
--- Create a database
+-- Create a database (the database)
 CREATE OR REPLACE DATABASE ICEBERG_DATABASE;
 
+-- We need to run the below commands (for storage integration) as an Admin
 USE ROLE accountadmin;
 
 -- Create our GCS volumne integration.  This will create a link between Snowflake and GCS. 
@@ -23,8 +25,29 @@ CREATE STORAGE INTEGRATION snowflake_integration
 -- Get the service principal that we will grant Storage Object Admin in our GCS bucket
 DESC STORAGE INTEGRATION snowflake_integration;
 
+-- Copy the above store principal name
 -- https://docs.snowflake.com/en/user-guide/tables-iceberg-configure-external-volume-gcs?_fsi=YFzl41ld&_fsi=YFzl41ld&_fsi=YFzl41ld
--- Object admin does not work
+-- Create a custom IAM role
+-- Create a custom role that has the permissions required to access the bucket and get objects.
+-- Log in to the Google Cloud Platform Console as a project editor.
+-- From the home dashboard, select IAM & Admin » Roles.
+-- Select Create Role.
+  -- Enter a Title and optional Description for the custom role.
+  -- Select Add Permissions.
+  -- In Filter, select Service and then select storage.
+    -- Filter the list of permissions, and add the following from the list:
+    -- storage.buckets.get
+    -- storage.objects.create
+    -- storage.objects.delete
+    -- storage.objects.get
+    -- storage.objects.list
+    -- Select Add.
+  -- Select Create.
+
+-- Assign the custom role to the GCS service account
+
+-- Open your GCS bucket (e.g. data-analytics-preview-snowflake) click on Permissions and add the service principal
+-- Select the role you just created
 
 -- Create an external volume on GCS
 -- https://docs.snowflake.com/en/sql-reference/sql/create-external-volume
@@ -42,13 +65,10 @@ CREATE EXTERNAL VOLUME snowflake_external_volume
 -- Describe the volume
 DESCRIBE EXTERNAL VOLUME snowflake_external_volume
 
-
--- Set it to use
+-- Set it to use the database
 USE ICEBERG_DATABASE;
   
--- Optional
---ALTER DATABASE ICEBERG_DATABASE SET EXTERNAL_VOLUME = 'snowflake_external_volume_001';
-
+-- Create and use a schema
 CREATE SCHEMA iceberg_schema;
 USE SCHEMA iceberg_schema;
 
@@ -59,23 +79,37 @@ CREATE ICEBERG TABLE driver_iceberg (driver_id int, driver_name string)
   EXTERNAL_VOLUME = 'snowflake_external_volume'
   BASE_LOCATION = 'driver_iceberg';
 
-
+-- This will show the table just created
 SHOW TABLES
 
-INSERT INTO driver_iceberg (driver_id, driver_name) VALUES (1, 'Adam');
+-- This will insert new data
+INSERT INTO driver_iceberg (driver_id, driver_name) VALUES (1, 'Driver 001');
 
+-- This will tell us the latest metadata json file that Snowflake is pointing to
+-- We need to point BigQuery to the same place
 SELECT JSON_EXTRACT_PATH_TEXT(
           SYSTEM$GET_ICEBERG_TABLE_INFORMATION('ICEBERG_DATABASE.iceberg_schema.driver_iceberg'),
           'metadataLocation')
 
           
 
-
+-----------------------------------------------------------------------------------------------
+-- Run this on BigQuery
+-----------------------------------------------------------------------------------------------
+-- The uris needs to be from the above Snowflake command
 CREATE OR REPLACE EXTERNAL TABLE `snowflake_managed_tables.driver_iceberg`
 OPTIONS (
   format = "ICEBERG",
   uris = ["gs://data-analytics-preview-snowflake/snowflake-volume/driver_iceberg/metadata/00001-3b2dc4b1-b873-48e4-ac4d-c1748e77dd4b.metadata.json"]
 );
 
-
+-- View the data in BQ
 SELECT * FROM `snowflake_managed_tables.driver_iceberg`;
+
+-- Now if you add or update data in Snowflake, BigQuery will not see it since we are pointing to a specific snapshot or metadata json
+-- You will need to run the "SYSTEM$GET_ICEBERG_TABLE_INFORMATION" command in Snowflake
+-- You will then need to update BigQuery
+-- https://cloud.google.com/bigquery/docs/iceberg-tables#update-table-metadata
+
+-- You can create a cloud function that Snowflake calls, run a cloud function on a timer that calls to Snowflake and then calls to BigQuery
+-- The approach is up to you.
