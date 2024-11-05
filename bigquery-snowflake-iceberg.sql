e6a350f815c87e417506d314499435f3e986e003
Adam Paternostro <adam.paternostro@outlook.com>
2024-11-04 19:37:46 -0500

    Updates to the step by step logic

--- bigquery-snowflake-iceberg.sql
+++ bigquery-snowflake-iceberg.sql
@@ -1,129 +1,245 @@
------------------------------------------------------------------------------------------------
--- Run this on BigQuery
------------------------------------------------------------------------------------------------
--- Create Cloud Resource Connection
--- BigLake tables access Google Cloud Storage data using a Cloud Resource connection. 
--- A connection can be associated with multiple tables within a project.
--- This command creates a connection named "my-connection" in the "US" location.
-bq mk --connection --location=US --project_id=my-project-id --connection_type=CLOUD_RESOURCE my-connection
+-- Step 1:
+-- Create a bucket to hold your BigLake Managed Table
+-- Open: https://console.cloud.google.com/storage/browser
+-- Click the Create Bucket button
+-- Enter your bucket name: blmt-snowflake-sharing (you can choose a different name)
+-- Click Next: Use Multi-region: us
+-- Click Create at the bottom
 
--- Create GCS bucket in the same region as the BigQuery dataset.  This bucket will hold the Parquet files.
--- Note: Replace `mybucket` with your desired bucket name.
 
--- Grant the BigLake connection service account necessary IAM permissions to access the GCS bucket.
--- The 'storage.admin' role grants full control over the bucket. Consider using a more restrictive role
--- like 'storage.objectAdmin' if only object-level access is required.
-gcloud storage buckets add-iam-policy-binding gs://mybucket --member=serviceAccount:my-service-account --role=roles/storage.admin
+-- Step 2:
+-- Navigate to BigQuery
+-- Open: https://console.cloud.google.com/bigquery
+-- Click the Add button
+-- Select "Connections to external data sources"
+   -- Select "Vertex AI remote models, remote functions and BigLake (Cloud Resource)"
+   -- Enter a name: blmt-connection (use the for friendly name and description)
 
--- Create a BigLake managed table named "driver".
--- This table uses the previously created Cloud Resource connection "biglake-notebook-connection".
--- It's configured to store data in Parquet format using the Iceberg table format.
--- Data will be stored in the specified GCS location.
-CREATE OR REPLACE TABLE `my-project-id.biglake_mt_dataset.driver`
+
+-- Step 3:
+-- Expand your project in the left hand panel
+-- Expand external connections
+-- Double click on us.blmt-connection
+-- Copy the service account id: e.g. bqcx-xxxxxxxxxxxx-s3rf@gcp-sa-bigquery-condel.iam.gserviceaccount.com
+
+
+-- Step 4:
+-- Open your storage account you created
+-- Open: https://console.cloud.google.com/storage/browser
+-- Click on: blmt-snowflake-sharing (or whatever you named it)
+-- Click on Permissions
+-- Click Grant Access
+-- Paste in the service account name
+-- For the role select Cloud Storage | Storage Object Admin [We want admin since we want Read/Write]
+-- Click Save
+
+
+-- Step 5:
+-- Navigate to BigQuery
+-- Open: https://console.cloud.google.com/bigquery
+-- Open a query window:
+-- Run this to create a dataset
+CREATE SCHEMA IF NOT EXISTS blmt_dataset OPTIONS(location = 'us');
+
+
+-- Step 6:
+-- Run this to create a BigLake Managed table (change the storage account name below)
+CREATE OR REPLACE TABLE `blmt_dataset.driver`
 (
-  driver_id                 INT64,
-  driver_name               STRING,
-  driver_mobile_number      STRING,
-  driver_license_number     STRING,
-  driver_email_address      STRING,
-  driver_dob                DATE,
-  driver_ach_routing_number STRING,
-  driver_ach_account_number STRING
+ driver_id                 INT64,
+ driver_name               STRING,
+ driver_mobile_number      STRING,
+ driver_license_number     STRING,
+ driver_email_address      STRING,
+ driver_dob                DATE,
+ driver_ach_routing_number STRING,
+ driver_ach_account_number STRING
 )
 CLUSTER BY driver_id
-WITH CONNECTION `my-project-id.us.biglake-notebook-connection`
+WITH CONNECTION `us.blmt-connection`
 OPTIONS (
-  file_format = 'PARQUET',
-  table_format = 'ICEBERG',
-  storage_uri = 'gs://data-analytics-preview-snowflake/biglake-managed-tables/driver'
+ file_format = 'PARQUET',
+ table_format = 'ICEBERG',
+ storage_uri = 'gs://blmt-snowflake-sharing/driver'
 );
 
 
--- Load data into the BigLake managed table from Parquet files in GCS.
--- Ensure the source data matches the table schema.
-LOAD DATA INTO `biglake_mt_dataset.driver`
+-- Step 7:
+-- Load the table with some data
+LOAD DATA INTO `blmt_dataset.driver`
 FROM FILES (
-  format = 'parquet',
-  uris = ['gs://data-analytics-golden-demo/biglake/v1-source/managed-table-source/driver/*.parquet']);
+ format = 'parquet',
+ uris = ['gs://data-analytics-golden-demo/biglake/v1-source/managed-table-source/driver/*.parquet']);
 
 
--- Export the Iceberg metadata for the BigLake managed table.
--- This generates metadata files in the "metadata" folder under the table's `storage_uri`.
--- This metadata is crucial for Iceberg's table management and time-travel capabilities.
-EXPORT TABLE METADATA FROM biglake_mt_dataset.driver
+-- View the data
+SELECT * FROM `blmt_dataset.driver` LIMIT 1000;
 
--- The Iceberg metadata is now located in the GCS location specified by the table's `storage_uri` within a "metadata" folder.
--- This path is important for any tools or systems that need to interact with the table using Iceberg.
--- EXPORT TABLE METADATA will update the version-hint.txt file that is understood by the Iceberg Hadoop catalog.
 
------------------------------------------------------------------------------------------------
--- Run this on Snowflake
------------------------------------------------------------------------------------------------
+-- Step 8:
+-- View the storage account
+-- Open: https://console.cloud.google.com/storage/browser and click on your storage account
+-- You should see a folder called "driver"
+-- You can  navigate to the "metadata" folder
+-- You will probably see the file "v0.metadata.json"
 
--- We need to run the below commands (for storage integration) as an Admin
+
+-- To export the most recent metadata run this in BigQuery (a seperate window is preferred)
+EXPORT TABLE METADATA FROM blmt_dataset.driver;
+
+
+-- In your storage window
+-- Press the Refresh button (top right)
+-- You will not see many files in the metadata folder
+
+
+
+
+-- Step 9:
+-- Let's connect the data to Snowflake
+-- Login to Snowflake
+-- Click the Create button on the top left
+-- Select SQL Worksheet
+-- Run this command to switch to an account admin since we have to run some commands that requires this role
 USE ROLE accountadmin;
 
--- Warehouse (this is the compute needed to run Snowflake)
--- https://docs.snowflake.com/en/sql-reference/sql/create-warehouse
-CREATE OR REPLACE WAREHOUSE ICEBERG_WAREHOUSE WITH WAREHOUSE_SIZE='XSMALL';
 
--- Create schema if not exist
-CREATE SCHEMA IF NOT EXISTS ICEBERG_SCHEMA;
-USE SCHEMA ICEBERG_SCHEMA;
-  
--- Drop existing storage integration and external volume to avoid conflicts
-USE SCHEMA INFORMATION_SCHEMA;
-USE DATABASE ICEBERG_LAB2;
-DROP ICEBERG TABLE IF EXISTS my_iceberg_table2;
-DROP STORAGE INTEGRATION IF EXISTS gcs_storage_integration;
-DROP EXTERNAL VOLUME IF EXISTS iceberg_volume;
+-- Step 10:
+-- Create a warehouse to hold the data
+CREATE OR REPLACE WAREHOUSE BLMT_WAREHOUSE WITH WAREHOUSE_SIZE='XSMALL';
 
--- Create our GCS volumne integration.  This will create a link between Snowflake and GCS. 
+
+-- Step 11:
+-- Create a database in snowflake
+CREATE OR REPLACE DATABASE BLMT_DATABASE;
+
+
+-- Step 12:
+-- Select the database
+USE DATABASE BLMT_DATABASE;
+
+
+-- Step 13:
+-- Create the schema to hold the table
+CREATE SCHEMA IF NOT EXISTS BLMT_SCHEMA;
+
+
+-- Step 14:
+-- Select the schema
+USE SCHEMA BLMT_SCHEMA;
+
+
+-- Step 15:
+-- Create our GCS volume integration.  This will create a link between Snowflake and GCS.
 -- A service principal will be created and we will grant access to our GCS bucket.
 -- https://docs.snowflake.com/en/sql-reference/sql/create-storage-integration
+-- Change the name of the storage account
 CREATE STORAGE INTEGRATION gcs_storage_integration
-  TYPE = EXTERNAL_STAGE
-  STORAGE_PROVIDER = 'GCS'
-  ENABLED = TRUE
-  STORAGE_ALLOWED_LOCATIONS = ('gcs://data-analytics-preview-snowflake/');
+ TYPE = EXTERNAL_STAGE
+ STORAGE_PROVIDER = 'GCS'
+ ENABLED = TRUE
+ STORAGE_ALLOWED_LOCATIONS = ('gcs://blmt-snowflake-sharing/');
 
+
+-- Step 16:
 -- Get the service principal that we will grant Storage Object Admin in our GCS bucket
 DESC STORAGE INTEGRATION gcs_storage_integration;
 
+-- Copy the STORAGE_GCP_SERVICE_ACCOUNT
+-- e.g. xxxxxxxxx@gcpuscentral1-1dfa.iam.gserviceaccount.com
+
+
+
+
+-- Step 17:
+-- https://docs.snowflake.com/en/user-guide/tables-iceberg-configure-external-volume-gcs?_fsi=YFzl41ld&_fsi=YFzl41ld&_fsi=YFzl41ld
+-- Create a custom IAM role
+-- Create a custom role that has the permissions required to access the bucket and get objects.
+-- Open: https://console.cloud.google.com/iam-admin/roles
+-- Select Create Role.
+ -- Enter a Title and optional Description for the custom role. [e.g. Snowflake Reader]
+ -- Select Add Permissions.
+ -- In Filter, select Service and then select storage.
+   -- Filter the list of permissions, and add the following from the list:
+   -- storage.buckets.get
+   -- storage.objects.get
+   -- storage.objects.list
+   -- Select Add.
+ -- Select Create.
+
+
+-- Step 18:
+-- Open your storage account you created
+-- Open: https://console.cloud.google.com/storage/browser
+-- Click on: blmt-snowflake-sharing (or whatever you named it)
+-- Click on Permissions
+-- Click Grant Access
+-- Paste in the service account name (from Snowflake)
+-- For the role select Custom | Snowflake Reader
+-- Click Save
+
+
+-- Step 19:
 -- Create an external volume on GCS
 -- https://docs.snowflake.com/en/sql-reference/sql/create-external-volume
-CREATE OR REPLACE EXTERNAL VOLUME iceberg_volume
-   STORAGE_LOCATIONS =
-      (
-         (
-            NAME = 'youriceberglab'
-            STORAGE_PROVIDER = 'GCS'
-            STORAGE_BASE_URL = 'gcs://data-analytics-preview-snowflake/'
-            )
-      );
+CREATE OR REPLACE EXTERNAL VOLUME gcs_volume
+  STORAGE_LOCATIONS =
+     (
+        (
+           NAME = 'gcs_volume'
+           STORAGE_PROVIDER = 'GCS'
+           STORAGE_BASE_URL = 'gcs://blmt-snowflake-sharing/'
+           )
+     );
 
--- Describe the volume
-DESCRIBE EXTERNAL VOLUME iceberg_volume
 
+-- Step 20:
 -- Create a catalog integration to manage Iceberg tables in the external volume
-CREATE OR REPLACE CATALOG INTEGRATION gcs_catalog_integration
-    CATALOG_SOURCE=OBJECT_STORE  -- Indicates that the catalog is backed by an object store
-    TABLE_FORMAT=ICEBERG        -- Specifies the table format as Iceberg
-    ENABLED=TRUE;               -- Enables the catalog integration
+CREATE OR REPLACE CATALOG INTEGRATION catalog_integration
+   CATALOG_SOURCE=OBJECT_STORE   -- Indicates that the catalog is backed by an object store
+   TABLE_FORMAT=ICEBERG          -- Specifies the table format as Iceberg
+   ENABLED=TRUE;                 -- Enables the catalog integration
 
--- Create a database to hold the Iceberg table
-CREATE DATABASE IF NOT EXISTS iceberg_lab2;
-USE DATABASE iceberg_lab2;
 
+-- Step 21:
 -- Create the Iceberg table, pointing to the existing metadata file
-CREATE OR REPLACE ICEBERG TABLE my_iceberg_table2
-    CATALOG='gcs_catalog_integration'   -- The catalog where the table will reside
-    EXTERNAL_VOLUME='iceberg_volume'      -- The external volume for table data
-    BASE_LOCATION=''                      -- Optional: Subdirectory within the storage location
-    METADATA_FILE_PATH='biglake-managed-tables/driver/metadata/v1.metadata.json'; -- Path to the existing metadata file
+-- To get the "latest" metadata file
+-- Open your storage account and navigate to the driver/metadata folder
+-- Open the version-hint.text, there will be a number inside
+-- Replace the number below (in the value of v173076288, but leave the "v")
+CREATE OR REPLACE ICEBERG TABLE driver
+   CATALOG='catalog_integration'   -- The catalog where the table will reside
+   EXTERNAL_VOLUME='gcs_volume'    -- The external volume for table data
+   BASE_LOCATION=''                -- Optional: Subdirectory within the storage location
+   METADATA_FILE_PATH='driver/metadata/v1730762887.metadata.json'; -- Path to the existing metadata file
 
+
+-- Step 22:
 -- This will show the table just created
 SHOW TABLES
 
--- Query the Iceberg table 
-SELECT * FROM my_iceberg_table2;
+
+-- Step 23:
+-- Query the Iceberg table
+SELECT * FROM driver;
+
+
+SELECT COUNT(*) FROM driver;
+
+
+-- Now that you are linked, you can try the following
+-- Insert a record into the BigQuery table:
+INSERT INTO `blmt_dataset.driver`
+(driver_id, driver_name, driver_mobile_number, driver_license_number, driver_email_address,
+driver_dob, driver_ach_routing_number, driver_ach_account_number)
+VALUES (0, 'New Driver', 'xxx-xxx-xxxx', 'driver_license_number', 'driver_email_address',
+CURRENT_DATE(), 'driver_ach_routing_number','driver_ach_account_number');
+
+
+-- Now Query the record in Snowflake
+SELECT * FROM driver WHERE driver_id = 0;
+
+
+– You will not see the new record
+-- You first need to tell BigQuery to export the latest metadata (Step 8).
+-- Update the metadata used by Snowflake (Step 21) pointing to the latest JSON file
--- snowflake-bigquery-iceberg.sql
+++ snowflake-bigquery-iceberg.sql
@@ -1,115 +1,195 @@
------------------------------------------------------------------------------------------------
--- Run this on Snowflake
------------------------------------------------------------------------------------------------
+-- Step 1:
+-- Login to Snowflake
+-- Click the Create button on the top left
+-- Select SQL Worksheet
+-- Run this command to switch to an account admin since we have to run some commands that requires this role
+USE ROLE accountadmin;
 
--- Warehouse (this is the compute needed to run Snowflake)
+
+-- Step 2:
+-- Create a warehouse to hold the data
 -- https://docs.snowflake.com/en/sql-reference/sql/create-warehouse
 CREATE OR REPLACE WAREHOUSE ICEBERG_WAREHOUSE WITH WAREHOUSE_SIZE='XSMALL';
 
+
+-- Step 3:
 -- Create a database (the database)
 CREATE OR REPLACE DATABASE ICEBERG_DATABASE;
 
--- We need to run the below commands (for storage integration) as an Admin
-USE ROLE accountadmin;
 
+-- Step 4:
+-- Create a bucket to hold your BigLake Managed Table
+-- Open: https://console.cloud.google.com/storage/browser
+-- Click the Create Bucket button
+-- Enter your bucket name: bigquery-snowflake-sharing (you can choose a different name)
+-- Click Next: Use Region: us-central1 <- must match your snowflake region
+-- Click Create at the bottom
+
+
+-- Step 5:
+-- In Snowflake
 -- Create our GCS volumne integration.  This will create a link between Snowflake and GCS. 
 -- A service principal will be created and we will grant access to our GCS bucket.
 -- https://docs.snowflake.com/en/sql-reference/sql/create-storage-integration
-CREATE STORAGE INTEGRATION snowflake_integration
+CREATE STORAGE INTEGRATION bigquery_integration
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'GCS'
   ENABLED = TRUE
-  STORAGE_ALLOWED_LOCATIONS = ('gcs://data-analytics-preview-snowflake')
+  STORAGE_ALLOWED_LOCATIONS = ('gcs://bigquery-snowflake-sharing')
 ;
 
--- Get the service principal that we will grant Storage Object Admin in our GCS bucket
-DESC STORAGE INTEGRATION snowflake_integration;
 
--- Copy the above store principal name
+-- Step 6:
+-- Get the service principal that we will grant Storage Object Admin in our GCS bucket
+DESC STORAGE INTEGRATION bigquery_integration;
+
+-- Copy the STORAGE_GCP_SERVICE_ACCOUNT
+-- e.g. xxxxxxxxx@gcpuscentral1-1dfa.iam.gserviceaccount.com
+
+
+-- Step 7:
 -- https://docs.snowflake.com/en/user-guide/tables-iceberg-configure-external-volume-gcs?_fsi=YFzl41ld&_fsi=YFzl41ld&_fsi=YFzl41ld
 -- Create a custom IAM role
 -- Create a custom role that has the permissions required to access the bucket and get objects.
--- Log in to the Google Cloud Platform Console as a project editor.
--- From the home dashboard, select IAM & Admin » Roles.
+-- Open: https://console.cloud.google.com/iam-admin/roles
 -- Select Create Role.
-  -- Enter a Title and optional Description for the custom role.
-  -- Select Add Permissions.
-  -- In Filter, select Service and then select storage.
-    -- Filter the list of permissions, and add the following from the list:
-    -- storage.buckets.get
-    -- storage.objects.create
-    -- storage.objects.delete
-    -- storage.objects.get
-    -- storage.objects.list
-    -- Select Add.
-  -- Select Create.
+ -- Enter a Title and optional Description for the custom role. [e.g. Snowflake Storage Admin]
+ -- Select Add Permissions.
+ -- In Filter, select Service and then select storage.
+   -- Filter the list of permissions, and add the following from the list:
+   -- storage.buckets.get
+   -- storage.objects.get
+   -- storage.objects.create
+   -- storage.objects.delete
+   -- storage.objects.list
+   -- Select Add.
+ -- Select Create.
 
--- Assign the custom role to the GCS service account
 
--- Open your GCS bucket (e.g. data-analytics-preview-snowflake) click on Permissions and add the service principal
--- Select the role you just created
+-- Step 8:
+-- Open your storage account you created
+-- Open: https://console.cloud.google.com/storage/browser
+-- Click on: bigquery-snowflake-sharing (or whatever you named it)
+-- Click on Permissions
+-- Click Grant Access
+-- Paste in the service account name (from Snowflake)
+-- For the role select Custom | Snowflake Storage Admin
+-- Click Save
 
+
+-- Step 9:
+-- In Snowflake
 -- Create an external volume on GCS
 -- https://docs.snowflake.com/en/sql-reference/sql/create-external-volume
-CREATE EXTERNAL VOLUME snowflake_external_volume
+CREATE EXTERNAL VOLUME snowflake_ext_volume
   STORAGE_LOCATIONS =
     (
       (
         NAME = 'us-central1'
         STORAGE_PROVIDER = 'GCS'
-        STORAGE_BASE_URL = 'gcs://data-analytics-preview-snowflake/snowflake-volume/'
+        STORAGE_BASE_URL = 'gcs://bigquery-snowflake-sharing/snowflake-volume/'
       )
     ),
     ALLOW_WRITES = TRUE;
 
+-- Step 10:
 -- Describe the volume
 DESCRIBE EXTERNAL VOLUME snowflake_external_volume
 
--- Set it to use the database
+-- Step 11:
+-- Set the current database
 USE ICEBERG_DATABASE;
   
--- Create and use a schema
+-- Step 12:
+-- Create a schema in Snowflake
 CREATE SCHEMA iceberg_schema;
+
+-- Step 13:
+-- Make the schema active
 USE SCHEMA iceberg_schema;
 
+-- Step 14:
 -- Create Iceberg table using Snowflake Catalog
 -- https://docs.snowflake.com/en/sql-reference/sql/create-iceberg-table-snowflake
-CREATE ICEBERG TABLE driver_iceberg (driver_id int, driver_name string)
+CREATE ICEBERG TABLE driver (driver_id int, driver_name string)
   CATALOG = 'SNOWFLAKE'
-  EXTERNAL_VOLUME = 'snowflake_external_volume'
-  BASE_LOCATION = 'driver_iceberg';
+  EXTERNAL_VOLUME = 'snowflake_ext_volume'
+  BASE_LOCATION = 'driver';
 
+-- Step 15:
 -- This will show the table just created
 SHOW TABLES
 
+-- Step 16:
 -- This will insert new data
-INSERT INTO driver_iceberg (driver_id, driver_name) VALUES (1, 'Driver 001');
+INSERT INTO driver (driver_id, driver_name) VALUES (1, 'Driver 001');
 
+SELECT * FROM driver;
+
+-- Step 17:
 -- This will tell us the latest metadata json file that Snowflake is pointing to
 -- We need to point BigQuery to the same place
-SELECT JSON_EXTRACT_PATH_TEXT(
-          SYSTEM$GET_ICEBERG_TABLE_INFORMATION('ICEBERG_DATABASE.iceberg_schema.driver_iceberg'),
-          'metadataLocation')
+SELECT REPLACE(JSON_EXTRACT_PATH_TEXT(
+          SYSTEM$GET_ICEBERG_TABLE_INFORMATION('ICEBERG_DATABASE.iceberg_schema.driver'),
+          'metadataLocation'), 'gcs://', 'gs://');
 
-          
+-- Step 18:
+-- Open your storage account you created
+-- Open: https://console.cloud.google.com/storage/browser
+-- Click on: bigquery-snowflake-sharing (or whatever you named it)
+-- You can now browser the iceberg files
 
------------------------------------------------------------------------------------------------
--- Run this on BigQuery
------------------------------------------------------------------------------------------------
+
+-- Step 19:
+-- Create a BigQuery Dataset
+CREATE SCHEMA IF NOT EXISTS snowflake_dataset OPTIONS(location = 'us-central1');
+
+
+-- Step 20:
+-- Navigate to BigQuery
+-- Open: https://console.cloud.google.com/bigquery
+-- Click the Add button
+-- Select "Connections to external data sources"
+   -- Select "Vertex AI remote models, remote functions and BigLake (Cloud Resource)"
+   -- Select region: us-central1
+   -- Enter a name: snowflake-connection (use the for friendly name and description)
+
+
+-- Step 21:
+-- Expand your project in the left hand panel
+-- Expand external connections
+-- Double click on us-central1.snowflake-connection
+-- Copy the service account id: e.g. bqcx-xxxxxxxxxxxx-s3rf@gcp-sa-bigquery-condel.iam.gserviceaccount.com
+
+
+-- Step 23:
+-- Open your storage account you created
+-- Open: https://console.cloud.google.com/storage/browser
+-- Click on: blmt-snowflake-sharing (or whatever you named it)
+-- Click on Permissions
+-- Click Grant Access
+-- Paste in the service account name
+-- For the role select Cloud Storage | Storage Object Viewer [Since Snowflake is the write BigQuery just reads]
+-- Click Save
+
+
+-- Step 24:
 -- The uris needs to be from the above Snowflake command
-CREATE OR REPLACE EXTERNAL TABLE `snowflake_managed_tables.driver_iceberg`
+CREATE OR REPLACE EXTERNAL TABLE `snowflake_dataset.driver`
+WITH CONNECTION `us-central1.snowflake-connection`
 OPTIONS (
   format = "ICEBERG",
-  uris = ["gs://data-analytics-preview-snowflake/snowflake-volume/driver_iceberg/metadata/00001-3b2dc4b1-b873-48e4-ac4d-c1748e77dd4b.metadata.json"]
+  uris = ["gs://bigquery-snowflake-sharing/snowflake-volume/driver/metadata/00001-2d763c77-df0a-4230-bd52-033877d02c40.metadata.json"]
 );
 
--- View the data in BQ
-SELECT * FROM `snowflake_managed_tables.driver_iceberg`;
 
+-- Step 25:
+-- View the data in BQ
+SELECT * FROM `snowflake_dataset.driver`;
+
+
+-- Step 26:
 -- Now if you add or update data in Snowflake, BigQuery will not see it since we are pointing to a specific snapshot or metadata json
 -- You will need to run the "SYSTEM$GET_ICEBERG_TABLE_INFORMATION" command in Snowflake
 -- You will then need to update BigQuery
 -- https://cloud.google.com/bigquery/docs/iceberg-tables#update-table-metadata
-
--- You can create a cloud function that Snowflake calls, run a cloud function on a timer that calls to Snowflake and then calls to BigQuery
--- The approach is up to you.
